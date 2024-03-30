b6ce32a45f6df5ae53f95ec2b7e8c5d31b0c9a7a
lactran1702 <lac.tran1702@gmail.com>
2024-03-30 15:37:43 +0700

    chore: fix eyes position

--- components/EyeTracker.tsx
+++ components/EyeTracker.tsx
@@ -42,21 +42,33 @@ function EyeTracker() {
   }
 
   return (
-    <main className="min-h-screen bg-white text-white" ref={mainRef}>
+    // Note: thằng bọc ngoài cùng bắt buộc phải có position để giữ cho 2 con mắt nằm trong phạm vi của con minion
+    // Em có thể giảm kích thước của con minion = nhiều cách. Mà scale là đỡ phải canh chỉnh 2 con mắt lại nhất
+    // Con mininon này có shadow nên nghĩ cách khác đi
+    <main className="relative" ref={mainRef}>
       <img
         id="anchor"
         src="https://raw.githubusercontent.com/ornelasedward/EyeFollow_effect/main/minion.png"
         alt="Minion"
+        className='relative z-10'
       />
-      <div id="eyes" className="absolute">
-        <img
-          className="eye"
+      {/* 
+      Note: chỉnh vị trì của 2 con mắt
+      Để dễ nhìn thì cho cái border xung quanh con mắt cho dể canh chỉnh, xong rồi thì xóa đi
+      Position của mắt phải thì giữ
+      Nhớ là z-index 2 con mắt phải lớn hơn con minion
+       */}
+      {/* 2 con mắt này ko phải hình tròn =)))))) */}
+      <div id="eyes" className="absolute z-20 flex top-[163px] left-[169px]   ">
+        <img id='eye-left'
+          className="eye border-red-600 border-2"
           src="https://raw.githubusercontent.com/ornelasedward/EyeFollow_effect/main/eye.png"
           style={{ bottom: 'calc(50% - 16px)', right: 'calc(50% - 16px)' }}
           alt="Eye"
         />
         <img
-          className="eye"
+          id='eye-right'
+          className="eye border-red-600 border-2 relative left-[29px] "
           src="https://raw.githubusercontent.com/ornelasedward/EyeFollow_effect/main/eye.png"
           style={{ bottom: 'calc(50% - 16px)', right: 'calc(50% - 248px)' }}
           alt="Eye"
